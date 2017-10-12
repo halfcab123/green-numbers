961749e624e75e322a501423a9cc096897b4ac46
Chris Ridgeway <chris.ridgeway01@gmail.com>
2017-10-12 13:04:48 +0000

    Update green-number.js

--- green-number.js
+++ green-number.js
@@ -1,26 +1,44 @@
-const isGreen = (n) => {
-  let count = 0
-  for(var i = 1; i < 10; i++){
-    if(testNums(i,i*i)){
+const getGreen = (n) => {
+  if(n===1 || n==='1'){ return '1' }
+  
+  let count = 1
+  let greenArray = ['1']
+  let currentGreen = '5'
+  
+  for(var i = 1; i <= 100; i++){
+    
+    let places = Math.pow(10,currentGreen.toString().split('').length)
+    
+    let test = parseInt(currentGreen) + (i * places)
+
+    if(testGreen(test)){
+      currentGreen = test
+      greenArray.push(currentGreen.toString())
       count++
+      i = 1
       if(count === n){
-        return i
+        return currentGreen
       }
     }
+
   }
+ 
+  return greenArray
   
-  return 'didn\'t find the nth green number with the current loop constraint'
 }
 
-const testNums = (n1,n2) => {
-  n1 = n1.toString().split('').reverse()
-  n2 = n2.toString().split('').reverse()
-  let green = false
+const testGreen = (n) => {
   
-  n1.forEach((val,idx)=>{
-    if(val === n2[idx]){
-      green = true
+  let n2 = (n * n).toString().split('').reverse()
+  n = n.toString().split('').reverse()
+   
+   let isGreen = true
+  
+  n.forEach((val,idx)=>{
+    if(val !== n2[idx]){
+      isGreen = false
     }
   })
-  return green
+  return isGreen
+
 }
